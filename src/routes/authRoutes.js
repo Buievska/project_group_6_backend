03e2d81e88bcca9273f6eccadd18cf15b44fc024
nasti <anastasiasaienko634@gmail.com>
2025-12-11 21:21:50 +0100

    update all

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import { logoutUser } from '../controllers/authController.js';
 
 const router = Router();
+
+//POST /auth/logout
+router.post('/auth/logout', logoutUser);
 
 export default router;
