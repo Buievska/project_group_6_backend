38bd00efc64838003dc994369d79d8dc9c62197d
SerhiiStepanovich <bysprint82@gmail.com>
2025-12-12 01:59:12 +0200

    add feedback_js

---
+++ src/models/feedback.js
@@ -0,0 +1,48 @@
+import { Schema, model } from 'mongoose';
+
+const feedbackSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      required: [true, 'Rating is required'],
+      min: 1,
+      max: 5,
+      default: 5,
+      index: true,
+    },
+    comment: {
+      type: String,
+      maxLength: 500,
+      trim: true,
+      default: '',
+    },
+
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+
+    tool: {
+      type: Schema.Types.ObjectId,
+      ref: 'Tool',
+      required: true,
+      index: true,
+    },
+
+    toolOwner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+feedbackSchema.index({ owner: 1, tool: 1 }, { unique: true });
+
+export const Feedback = model('Feedback', feedbackSchema);
