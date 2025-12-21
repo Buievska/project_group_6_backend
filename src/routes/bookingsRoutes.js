3b3da775ee7b29fece8439116b482267e4ca5c50
Ania <anna111239@gmail.com>
2025-12-21 02:45:37 +0100

    booking user

--- src/routes/bookingsRoutes.js
+++ src/routes/bookingsRoutes.js
@@ -2,12 +2,17 @@ import { Router } from 'express';
 import { authenticate } from '../middlewares/authenticate.js';
 import { celebrate } from 'celebrate';
 import { bookingValidator } from '../validation/bookingValidation.js';
-import { bookingController } from '../controllers/bookingsController.js';
+import {
+  bookingController,
+  getUserBookingsController,
+} from '../controllers/bookingsController.js';
 
 const router = Router();
 
 router.use(authenticate);
 
 router.post('/', celebrate(bookingValidator), bookingController);
+
+router.get('/my', getUserBookingsController);
 
 export default router;
